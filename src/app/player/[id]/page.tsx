0150a1a5bc68f4070cdeee9ad332fa7b8be9f5a6
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-03-14 22:33:39 -0700

    built out basic player stat card

--- src/app/player/[id]/page.tsx
+++ src/app/player/[id]/page.tsx
@@ -19,10 +19,19 @@ const Stats = ({ params }: IdParams) => {
     })
   }, [])
   return (
-    <div className='flex h-10'>
-      <p className='text-white'>
-        {player.name}
-      </p>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      {/* <img className="w-full" src="" alt={legend.bio_name} /> */}
+      <div className ="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{player.name}</div>
+        <p className="text-gray-700 text-base">
+          {player.brawlhalla_id}
+        </p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Games: {player.games}</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Wins: {player.wins}</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Losses: {player.games - player.wins}</span>
+      </div>
     </div>
   )
 }
