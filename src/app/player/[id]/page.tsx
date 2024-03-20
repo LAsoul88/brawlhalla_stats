96886601d63ba2e77191a98efb18df2c0638d184
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-03-19 19:23:36 -0700

    converting ranked page into full on ranked/stats page for each player, reformatted api call structure

--- src/app/player/[id]/page.tsx
+++ src/app/player/[id]/page.tsx
@@ -2,9 +2,11 @@
 import { useState, useEffect } from 'react'
 
 const Stats = ({ params }: IdParams) => {
-  const [player, setPlayer] = useState<Player>({} as Player)
+  const [player, setPlayer] = useState({} as Player)
+  const [stats, setStats] = useState({} as PlayerGeneral)
+  const [ranked, setRanked] = useState({} as PlayerRanked)
   const url: string = process.env.NEXT_PUBLIC_API || ''
-  
+
   useEffect(() => {
     fetch(url, {
       method: 'POST',
@@ -13,30 +15,34 @@ const Stats = ({ params }: IdParams) => {
       },
       body: JSON.stringify({
         player_id: params.id,
-        endpoint: 'legends'
+        endpoint: 'stats'
       },
     )})
     .then(res => res.json())
     .then(data => {
-      setPlayer(data)
-      console.log(data)
+      setStats(JSON.parse(data['stats']))
+      setRanked(JSON.parse(data['ranked']))
+      console.log(data['stats'])
     })
   }, [])
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      {/* <img className="w-full" src="" alt={legend.bio_name} /> */}
-      <div className ="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{player.name}</div>
-        <p className="text-gray-700 text-base">
-          {player.brawlhalla_id}
-        </p>
+    <>
+      <h2>{ranked.name}'s Stats</h2>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        {/* <img className="w-full" src="" alt={legend.bio_name} /> */}
+        <div className ="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Ranked</div>
+          <p className="text-gray-700 text-base">
+            {ranked.brawlhalla_id}
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Games: {ranked.games}</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Wins: {ranked.wins}</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Losses: {ranked.games - ranked.wins}</span>
+        </div>
       </div>
-      <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Games: {player.games}</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Wins: {player.wins}</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Losses: {player.games - player.wins}</span>
-      </div>
-    </div>
+    </>
   )
 }
 
