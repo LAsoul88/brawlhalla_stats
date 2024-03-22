effdd4cd31eedbe20f99ccdff4cd7fbdfce605da
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-03-22 14:38:46 -0700

    added leader info and fill in table for no data

--- src/app/(components)/Leaderboard.tsx
+++ src/app/(components)/Leaderboard.tsx
@@ -48,80 +48,44 @@ const Leaderboard = () => {
           <tbody>
             { board.length > 0 ? (board.map(leader => {
                 return (
-                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={leader.brawlhalla_id}>
                     <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Apple MacBook Pro 17"
+                      {leader.name}
                     </th>
                     <td className="px-6 py-4">
-                        Silver
+                      {leader.region}  
                     </td>
                     <td className="px-6 py-4">
-                        Laptop
+                      {leader.rank}
                     </td>
                     <td className="px-6 py-4">
-                        $2999
+                      {leader.games}
                     </td>
                     <td className="px-6 py-4">
-                        $2999
+                      {leader.wins}
                     </td>
                   </tr>
                 )
               })) : (
-                ''
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    N/A
+                  </th>
+                  <td className="px-6 py-4">
+                    N/A  
+                  </td>
+                  <td className="px-6 py-4">
+                    N/A
+                  </td>
+                  <td className="px-6 py-4">
+                    N/A
+                  </td>
+                  <td className="px-6 py-4">
+                    N/A
+                  </td>
+                </tr>
               )
-
             }
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  Apple MacBook Pro 17"
-              </th>
-              <td className="px-6 py-4">
-                  Silver
-              </td>
-              <td className="px-6 py-4">
-                  Laptop
-              </td>
-              <td className="px-6 py-4">
-                  $2999
-              </td>
-              <td className="px-6 py-4">
-                $2999
-              </td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  Microsoft Surface Pro
-              </th>
-              <td className="px-6 py-4">
-                  White
-              </td>
-              <td className="px-6 py-4">
-                  Laptop PC
-              </td>
-              <td className="px-6 py-4">
-                  $1999
-              </td>
-              <td className="px-6 py-4">
-                $2999
-              </td>
-            </tr>
-            <tr className="bg-white dark:bg-gray-800">
-              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">
-                  Black
-              </td>
-              <td className="px-6 py-4">
-                  Accessories
-              </td>
-              <td className="px-6 py-4">
-                  $99
-              </td>
-              <td className="px-6 py-4">
-                $2999
-              </td>
-            </tr>
           </tbody>
         </table>
       </div>
